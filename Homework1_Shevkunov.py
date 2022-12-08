e1b557115fde53f1d2cc9659955a2f899cfa87ff
VladShevkunov <Shevkunovsvs@gmail.com>
2022-12-08 19:17:44 +0200

    homework1

---
+++ Homework1_Shevkunov.py
@@ -0,0 +1,22 @@
+print('import antigravity')
+print('import __hello__')
+print('from __future__ import braces')
+print('''
+    *\t          * *                   *        **********
+     *\t         *  *                  * *       *          *
+      *\t        *   *                 *   *      *           *
+       *\t   *    *                *     *     *            *
+        *     *     *               *       *    *            *
+         *   *      *              ***********   *           *
+          * *       *             *           *  *          *
+           *        ************ *             * **********
+''')
+
+print('''Escape sequences
+\\a      Bell (alert)
+\\b      Backspace
+\\n      New line
+\\t      Horizontal tab
+\\\      Backslash 
+\\"      Double quotation mark "
+\'      Single quotation mark ''''')
